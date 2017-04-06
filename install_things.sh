db863cf6443517c1b01e2240325a78e3a0c95479
mbootcamp <bootcamp.mibura.2017@gmail.com>
2017-04-05 19:31:33 -0700

    Update install_things.sh

--- install_things.sh
+++ install_things.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 curl -sSL https://get.docker.com/ | sh
-apt-get update
-apt-get install python-pip
+apt-get update -y
+apt-get install python-pip -y
 pip install docker-compose
 cp systemd/* /etc/systemd/system
