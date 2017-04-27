ed8d1e708db1f55b9aff9e86ffa8fb8a9af00f7e
Ubuntu <ubuntu@ip-172-31-9-255.us-west-2.compute.internal>
2017-04-27 04:25:09 +0000

    render templates cooler with javascript

--- uwsgi_redis/myapp.py
+++ uwsgi_redis/myapp.py
@@ -1,16 +1,31 @@
 import redis
-from flask import Flask
-application = Flask(__name__)
+from flask import Flask, render_template
+application = app = Flask(__name__)
+
+@app.route('/fetch')
+def fetch():
+    return '''[
+             {"url": "http://www.ucla.edu", "display": "UCLA"},
+             {"url": "https://www.kernel.org", "display": "KERNEL"},
+             {"url": "http://flask.pocoo.org/", "display": "FLAsk"}
+            ]'''
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name, footer="foot")
 
 @application.route("/")
-def hello():
+def index():
     r = redis.Redis('redis')
     try:
         r.get('counter')
     except:
         r.set('counter', 0)
     r.incr('counter')
-    return "<h1>This page has been visited %s times!</h1>" % r.get('counter')
+    counter = r.get('counter')
+    return render_template('index.html', counter=counter)
+    #return "<h1>This page has been visited %s times!</h1>" % r.get('counter')
 
 @application.route("/clear")
 def blank():
@@ -19,5 +34,6 @@ def blank():
     return "<h1>CLEARED COUNTER TO: %s</h1>" % r.get('counter')
 
 if __name__ == "__main__":
+    application.config['debug'] = True
     application.run(host='0.0.0.0')
 
