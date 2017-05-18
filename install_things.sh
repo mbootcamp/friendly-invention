6411cc99a1ea11a7b1f389d31cf3c1cb5e9a3cf0
Ubuntu <ubuntu@ip-172-31-12-56.us-west-2.compute.internal>
2017-05-18 04:28:10 +0000

    adding postgresql

--- install_things.sh
+++ install_things.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 curl -sSL https://get.docker.com/ | sudo sh
 sudo apt-get update -y
-sudo apt-get install python-pip -y
+sudo apt-get install python-pip postgresql postgresql-contrib -y
 sudo pip install docker-compose
 sudo cp systemd/* /etc/systemd/system
 sudo usermod -aG docker $USER
