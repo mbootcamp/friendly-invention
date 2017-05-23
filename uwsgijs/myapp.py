59507247ef27a37a3f14d143e4cf256841a1c600
Ubuntu <ubuntu@ip-172-31-12-56.us-west-2.compute.internal>
2017-05-23 05:18:31 +0000

    updates from past week, adding more postgres support j

--- uwsgijs/myapp.py
+++ uwsgijs/myapp.py
@@ -1,20 +1,25 @@
 import redis
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, jsonify
+from sqlalchemy import create_engine
+
 application = app = Flask(__name__)
+pgdb = create_engine('postgresql://ubuntu@/ubuntu')
 
 @app.route('/blog_list.json')
 @app.route('/fetch')
 def fetch():
     # should grab paginated results from db
-    return '''[
-             {"url": "http://www.ucla.edu", "display": "UCLA"},
-             {"url": "https://www.kernel.org", "display": "KERNEL"},
-             {"url": "http://flask.pocoo.org/", "display": "FLAsk"}
-            ]'''
+    top_posts = pgdb.execute('SELECT id,title FROM posts ORDER BY id DESC LIMIT 10')
+    return jsonify([{ 'url': url_for('post', post_id=row[0]), 'display': row[1]}
+        for row in top_posts ])
+
 
 def _get_post(post_id):
     # TODO: fetch from database
-    return {'content': 'content '*24, 'title': 'title '*3}
+    query = 'SELECT title,body FROM posts WHERE id=%s LIMIT 1' % post_id
+    post = pgdb.execute(query).fetchone()
+    return {'title': post[0], 'body': post[1]}
+
 
 # TODO: homework
 @app.route('/post/<int:post_id>')
@@ -22,28 +27,51 @@ def post(post_id):
     post = _get_post(post_id)
     return render_template('post.html', post=post)
 
+
 @app.route('/hello/')
 @app.route('/hello/<name>')
 def hello(name=None):
     return render_template('hello.html', name=name, footer="foot")
 
+
 @application.route("/about")
 def about():
     return render_template('about.html')
 
-@application.route("/post")
-def new_post():
-    return render_template('forms.html', nav_name="post")
 
-@application.route("/")
+#@app.route('/login', methods=['GET', 'POST'])
+#def login():
+#    if request.method == 'POST':
+#        do_the_login()
+#    else:
+#        show_the_login_form()
+@application.route("/post", methods=['GET', 'POST'])
+def new_post():
+    if request.method == 'POST':
+        # request.form['title'] to access form title
+        # insert the post
+        return 'implemented as homework' + request.form['title']
+        # redirect to the post
+    else:
+        return render_template('forms.html', nav_name="post")
+
+
+@application.route("/", methods=['HEAD','GET','SOMERANDOMSTUFF'])
 def index():
     return render_template('index.html', nav_name="blog")
+
 
 @application.route("/clear")
 def blank():
     r = redis.Redis('redis')
     r.set('counter', 0)
     return "<h1>CLEARED COUNTER TO: %s</h1>" % r.get('counter')
+
+
+@application.route("/postvar")
+def postcars():
+    return render_template('postvar.html')
+
 
 if __name__ == "__main__":
     application.config['debug'] = True
---
+++ uwsgijs/test.py
@@ -0,0 +1,23 @@
+def my_decorator(some_function):
+
+    def wrapper():
+
+        num = 10
+
+        if num == 10:
+            print("Yes!")
+        else:
+            print("No!")
+
+        some_function()
+
+        print("Something is happening after some_function() is called.")
+
+    return wrapper
+
+@my_decorator
+def test():
+    print('test my decorator test()')
+
+if __name__ == "__main__":
+    test()
